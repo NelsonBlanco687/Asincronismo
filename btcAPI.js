45062c77f4ada3f71d267fc00b8cdbceb30325ae
NelsonBlanco687 <nelsonblanco687@gmail.com>
2022-05-03 13:31:54 -0600

    consumiendo api sobre btc

---
+++ btcAPI.js
@@ -0,0 +1,12 @@
+const fetch = require('node-fetch');
+
+fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then((response) =>{
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data)
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
