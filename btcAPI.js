1db99b14c31e60ebba066fb488d501ac00a332e1
NelsonBlanco687 <nelsonblanco687@gmail.com>
2022-05-03 14:31:42 -0600

    Consumiendo la api sobre precios de btc

--- btcAPI.js
+++ btcAPI.js
@@ -1,12 +1,27 @@
 const fetch = require('node-fetch');
+const fs = require('fs');
 
-fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-    .then((response) =>{
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data)
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
+    async function getPricesbtc(){
+        try {
+            const response= await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+            const data =await response.json();
+            let btcprice = "";
+            Array.from(data).forEach(precios=> {
+                btcprice+= `${precios['time']}, ${precios['chartName']},${precios['code']},${precios['rate']},${precios['description']}`;
+                
+            });
+            fs.writeFile('precios.txt', btcprice, (error) =>{
+                if(error){
+                    console.log(error);
+                    return;
+                }
+                console.log('se ha creado el archivo');
+            } )
+
+
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    getPricesbtc();
